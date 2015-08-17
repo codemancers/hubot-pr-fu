d15b468d3d3c4801e8220ef6a502c9dce2be7f80
Kashyap <kashyap.kmbc@gmail.com>
2015-08-17 16:23:40 +0530

    Update format for stats command
    This is how it would look:
    
    11 open PRs
    
    6 by wendy0402
    2 by Paxa
    3 by saveav
    
    10 mergeable
    1 unmergeable
    
    219 Refactor refund method in payment controller,put it in different object
    Assigned to : Not assigned
    Opened by   : wendy0402

--- ruby_app/app.rb
+++ ruby_app/app.rb
@@ -8,7 +8,7 @@ Octokit.default_media_type = "application/vnd.github.beta+json"
 
 
 client = Octokit::Client.new access_token: GH_AUTH_TOKEN
-repo   = client.repo "veritrans/turbo"
+repo   = client.repo "#{HUBOT_VT_GITHUB_ORG}/#{HUBOT_VT_GITHUB_REPO}"
 pulls  = repo.rels[:pulls].get.data
 
 open_pulls        = pulls.select { |x| x[:state] = "open" }
@@ -17,12 +17,12 @@ open_pulls        = pulls.select { |x| x[:state] = "open" }
 aggregated_data = open_pulls.map do |pull|
   pull_request_data = pull.rels[:self].get.data
   {
-    title: pull_request_data[:title],
-    mergeable: pull_request_data[:mergeable] || "Unspecified",
-    assignee: pull_request_data[:assignee] || "Not assigned",
-    number: pull_request_data[:number],
-    opened_by: pull_request_data[:user][:login],
-    html_url: pull_request_data[:html_url],
+    title:      pull_request_data[:title],
+    mergeable:  pull_request_data[:mergeable] || "Unspecified",
+    assignee:   pull_request_data[:assignee] || "Not assigned",
+    number:     pull_request_data[:number],
+    opened_by:  pull_request_data[:user][:login],
+    html_url:   pull_request_data[:html_url],
     created_at: pull_request_data[:created_at]
   }
 end
@@ -31,18 +31,35 @@ mergeable_pulls   = aggregated_data.select { |x| x[:mergeable] == true }
 unmergeable_pulls = aggregated_data.select { |x| x[:mergeable] != true }
 
 puts <<EOF
-  Mergeable PRs       : #{mergeable_pulls.count}
-  PRs with conflicts  : #{unmergeable_pulls.count}
+  #{open_pulls.count} open PRs
 EOF
 
-unmergeable_pulls.each do |pull|
-puts "\n\n"
+puts "\n"
+
+aggregated_data.group_by { |x| x[:opened_by] }.each do |k, v|
+  puts <<EOF
+  #{v.count} by #{k}
+EOF
+end
+
+puts "\n"
+
 puts <<EOF
+  #{mergeable_pulls.count} mergeable
+  #{unmergeable_pulls.count} unmergeable
+EOF
+
+puts "\n"
+
+unmergeable_pulls.each do |pull|
+  puts "\n"
+  puts <<EOF
   #{pull[:number]} #{pull[:title]}
   Assigned to : #{pull[:assignee]}
   Opened by   : #{pull[:opened_by]}
 EOF
 end
+
 
 # Stats
 #
